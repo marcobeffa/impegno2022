b756077a306bb46ed38656f7c16d48691c94ad4d
marcobeffa <marcobeffa@gmail.com>
2022-08-25 17:05:07 +0200

    aggiunto validatazioni

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,6 @@ class Profile < ApplicationRecord
 
   has_many :notes
 
-  validates :username, uniqueness: true, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/,
-    message: "only allows letters" }
+  validates :username, uniqueness: true, presence: true, format: { with: /\A[a-z0-9_]+\z/,
+    message: "può contenere solo lettere minuscole e numeri e _" }
 end
